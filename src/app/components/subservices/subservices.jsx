6f1b9db69d07c6245eb87378ae2d7e5073553485
anitasharmaca <anitasharmaca19@gmail.com>
2023-12-01 21:23:23 +0530

    responsiveness

--- src/app/components/subservices/subservices.jsx
+++ src/app/components/subservices/subservices.jsx
@@ -9,7 +9,7 @@ const Subservices = ({ subtitle, title, content, imageUrl}) => {
   <>
       <div className={styles.container}>
         <div className={styles.service}>
-            <div className={styles.main}>
+            <div className={styles.mainLeft}>
                 <h6>{subtitle}</h6>
                 <h2>{title}</h2>
                 <p>{content}</p>
@@ -18,33 +18,33 @@ const Subservices = ({ subtitle, title, content, imageUrl}) => {
                 </div>
             </div>
             <div className={styles.mainRight}>
-                <img src={imageUrl} />
-                <div className={styles.mainLeft}>                      
+                <img src='assets/hero.png' />
+                <div className={styles.content}>
                         <div className={styles.bullet}>
-                          <div><svg  width="12" height="12" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <div><svg  width="14" height="14" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M16.0989 0.000102093C17.7011 0.000110658 19 1.29897 19 2.9012V15.7445C19 18.3593 15.8126 19.6396 14.0039 17.7513L1.70129 4.90794C-0.0658543 3.06312 1.24168 2.26738e-05 3.79633 3.63296e-05L16.0989 0.000102093Z" fill="#090909"/>
                           </svg></div>
                           <div><p>Total control with full root access and optional control panels.</p></div>
-                        </div>                    
-                        <div className={styles.bullet}>
-                          <div><svg  width="12" height="12" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+                        </div> <div className={styles.bullet}>
+                          <div><svg  width="14" height="14" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M16.0989 0.000102093C17.7011 0.000110658 19 1.29897 19 2.9012V15.7445C19 18.3593 15.8126 19.6396 14.0039 17.7513L1.70129 4.90794C-0.0658543 3.06312 1.24168 2.26738e-05 3.79633 3.63296e-05L16.0989 0.000102093Z" fill="#090909"/>
                           </svg></div>
                           <div><p>Automated daily backups and on-demand snapshots.</p></div>
-                        </div>
-                        <div className={styles.bullet}>
-                          <svg  width="12" height="12" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+                        </div> <div className={styles.bullet}>
+                          <div><svg  width="14" height="14" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M16.0989 0.000102093C17.7011 0.000110658 19 1.29897 19 2.9012V15.7445C19 18.3593 15.8126 19.6396 14.0039 17.7513L1.70129 4.90794C-0.0658543 3.06312 1.24168 2.26738e-05 3.79633 3.63296e-05L16.0989 0.000102093Z" fill="#090909"/>
-                          </svg>
-                          <p>99.9% uptime guaranteed.</p>
-                        </div>
-                        <div className={styles.bullet}>
-                          <svg  width="12" height="12" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          </svg></div>
+                          <div><p>99.9% uptime guaranteed.</p></div>
+                          
+                        </div> <div className={styles.bullet}>
+                          <div><svg  width="14" height="14" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M16.0989 0.000102093C17.7011 0.000110658 19 1.29897 19 2.9012V15.7445C19 18.3593 15.8126 19.6396 14.0039 17.7513L1.70129 4.90794C-0.0658543 3.06312 1.24168 2.26738e-05 3.79633 3.63296e-05L16.0989 0.000102093Z" fill="#090909"/>
-                          </svg>
-                          <p>99.9% uptime guaranteed.</p>
+                          </svg></div>
+                          <div><p>99.9% uptime guaranteed.</p></div>
                         </div>
-                  </div>
+                </div>
             </div>
         </div>
 </div>
