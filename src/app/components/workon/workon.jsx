33261aa2097213eecf167139edb7fc827e11cad4
anitasharmaca <anitasharmaca19@gmail.com>
2024-01-19 13:30:51 +0530

    update working on component

--- src/app/components/workon/workon.jsx
+++ src/app/components/workon/workon.jsx
@@ -14,10 +14,34 @@ const workon = () => {
         <p>Dribble</p>
         <h3>Food Recipe & Ingredients Concept Recipe App</h3>
     </div>
-    <div className={styles.story} style={{marginTop: '160px'}}>
+  <div className={`${styles.story} ${styles.design}`}>
     <img src="/assets/story/secondst.png" />
     <p>Dribble</p>
-        <h3>Food Recipe & Ingredients Concept Recipe App</h3>
+        <h3>U2 Fashion Store| Fashion App | UI/UX design</h3>
+    </div>
+    </div>
+    <div className={styles.main}>
+    <div className={`${styles.story} ${styles.designing}`}>
+        <img src="/assets/story/thirdst.png" />
+        <p>Dribble</p>
+        <h3>Watch Retail App E-Commerce Mobile App Buying and selling goods</h3>
+    </div>
+  <div className={`${styles.story}`}>
+    <img src="/assets/story/fourst.png" />
+    <p>Dribble</p>
+        <h3>Concept of Mobile App for order and delivery of Lamps</h3>
+    </div>
+    </div>
+    <div className={styles.main}>
+    <div className={`${styles.story} ${styles.designing}`}>
+        <img src="/assets/story/fifthst.png" />
+        <p>Dribble</p>
+        <h3>Kakud- Fertilizer & Pesticides E-commerce App | Mobile App</h3>
+    </div>
+  <div className={`${styles.story}`}>
+    <img src="/assets/story/sixthst.png" />
+    <p>Dribble</p>
+        <h3>Ajayal - E-commerce Mobile App Development | UI/UX design</h3>
     </div>
     </div>
     </div>
