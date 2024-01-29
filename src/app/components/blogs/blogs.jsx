71ab66467e00273b643c024f2b8d6274eb8228f2
swathi <swathiashok94@gmail.com>
2024-01-29 13:04:35 +0530

    ui home bugs

--- src/app/components/blogs/blogs.jsx
+++ src/app/components/blogs/blogs.jsx
@@ -20,7 +20,7 @@ const blogs = () => {
         <button className={menu===2 ? `${styles.menu} ${styles.active}` : styles.menu} onClick={()=>handleClick(2)}>Insights</button>
         </div>
         <button className={styles.view}><p>View all</p>
-        <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 23 23" fill="none">
   <g clip-path="url(#clip0_1126_1732)">
     <path d="M11.313 22.1921L8.84144 19.7418L15.3294 13.2539H0.126953V9.65305H15.3294L8.84144 3.17578L11.313 0.714844L22.0517 11.4535L11.313 22.1921Z" fill="#1D212C"/>
   </g>
--- src/app/components/newintegration/newintegration.jsx
+++ src/app/components/newintegration/newintegration.jsx
@@ -6,63 +6,46 @@ import styles from './newintegration.module.css'
 
 const Integration = ({ subtitle, title, content, growi, growi2, growi3, growi4, growi5, growi6,}) => {
   return (
-      <div className={styles.container}>
-            
-        <div className={styles.contentleft}>
-                <h2>{title}</h2>
-                <p>{content}</p>
-                <div>
-                  <Darkbutton title="Lets connect"/>
+      <div className={styles.outercont}>
+             <div className={styles.container}>
+                <div className={styles.contentleft}>
+                    <h2>{title}</h2>
+                    <p>{content}</p>
+                    <div>
+                    <Darkbutton title="Lets connect"/>
+                    </div>
+                </div>
+                <div className={styles.mainRight}>
+                    <div className={styles.mainRight1}>
+                        <div className={styles.techicon}>
+                            <img src={growi} />
+                            <span>Ship Rocket</span>
+                        </div>
+                        <div className={styles.techicon}>
+                            <img src={growi2} />
+                            <span>CC Avenue</span>
+                        </div>
+                        <div className={styles.techicon}>
+                            <img src={growi3} />
+                            <span>Paytm</span>
+                        </div>
+                    </div>
+                    <div className={styles.mainRight2}>
+                        <div className={styles.techicon}>
+                            <img src={growi4} />
+                            <span>Razor Pay</span>
+                        </div>
+                        <div className={styles.techicon}>
+                            <img src={growi5} />
+                            <span>Phonepay</span>
+                        </div>
+                        <div className={styles.techicon}>
+                            <img src={growi6} />
+                            <span>Paypal</span>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className={styles.mainRight}>
-                <div className={styles.mainRight1}>
-                    <div className={styles.techicon}>
-                        <img src={growi} />
-                        <span>Ship Rocket</span>
-                    </div>
-                    <div className={styles.techicon}>
-                        <img src={growi2} />
-                        <span>CC Avenue</span>
-                    </div>
-                    <div className={styles.techicon}>
-                        <img src={growi3} />
-                        <span>Paytm</span>
-                    </div>
-                </div>
-                <div className={styles.mainRight2}>
-                    <div className={styles.techicon}>
-                        <img src={growi4} />
-                        <span>Razor Pay</span>
-                    </div>
-                    <div className={styles.techicon}>
-                        <img src={growi5} />
-                        <span>Phonepay</span>
-                    </div>
-                    <div className={styles.techicon}>
-                        <img src={growi6} />
-                        <span>Paypal</span>
-                    </div>
-                </div>
-            </div>
-           {/*  <div className={styles.mainRightxs}>
-                    <div className={styles.techicon}>
-                        <img src={iwebflow} />
-                        <span>Webflow</span>
-                    </div>
-                    <div className={styles.techicon}>
-                        <img src={iwoocom} />
-                        <span>WooCommerce</span>
-                    </div>
-                    <div className={styles.techicon}>
-                        <img src={imegento} />
-                        <span>Meganto</span>
-                    </div>
-                    <div className={styles.techicon}>
-                        <img src={ishopify} />
-                        <span>Shopify</span>
-                    </div>
-            </div> */}
 </div>
   )
 }
